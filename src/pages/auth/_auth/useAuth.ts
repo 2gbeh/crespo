bc3428ca7d9fecffe89856f8654d9f9d1676465c
2gbeh <tugbeh.osaretin@gmail.com>
2024-01-30 09:40:14 +0100

    update tsconfig lint rules

--- src/pages/auth/_auth/useAuth.ts
+++ src/pages/auth/_auth/useAuth.ts
@@ -1,11 +1,11 @@
-import { FormEvent, useRef, useState } from "react";
+import { ChangeEvent, MutableRefObject, useRef, useState } from "react";
 
 export default function useAuth(initialFormData: Record<string, string>) {
-  const passwordRef = useRef(null);
+  const passwordRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   const [formData, setFormData] = useState(initialFormData);
   const [showPassword, setShowPassword] = useState(false);
   //
-  function updateFormData(ev: FormEvent) {
+  function updateFormData(ev: ChangeEvent<HTMLInputElement>) {
     setFormData({
       ...formData,
       [ev.target.name]: ev.target.value.trim(),
@@ -13,8 +13,8 @@ export default function useAuth(initialFormData: Record<string, string>) {
   }
 
   function togglePassword() {
-    let { type } = passwordRef.current;
-    passwordRef.current.type = type === "password" ? "text" : "password";
+    // let { type } = passwordRef.current;
+    passwordRef.current.type = '' === "password" ? "text" : "password";
     setShowPassword((prev) => !prev);
   }
   //
