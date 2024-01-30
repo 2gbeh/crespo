4f1bfdbffad5d1ec254d698b003a1698f717fa4a
2gbeh <tugbeh.osaretin@gmail.com>
2024-01-30 09:23:39 +0100

    fix error boundary type def

--- src/pages/auth/AuthError.tsx
+++ src/pages/auth/AuthError.tsx
@@ -1,15 +1,16 @@
 import { useRouteError } from "react-router-dom";
 
 export default function AuthError() {
-  const error = useRouteError();
-  console.log("🚀 ~ AuthError ~ error:", error)
+  const error: unknown = useRouteError();
+  console.log("🚀 ~ AuthError ~ error:", error);
 
   return (
     <div>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        {(error as Error)?.message ||
+          (error as { statusText?: string })?.statusText}
       </p>
     </div>
   );
--- src/pages/dashboard/DashboardError.tsx
+++ src/pages/dashboard/DashboardError.tsx
@@ -1,15 +1,16 @@
 import { useRouteError } from "react-router-dom";
 
 export default function DashboardError() {
-  const error = useRouteError();
-  console.log("🚀 ~ DashboardError ~ error:", error)
+  const error: unknown = useRouteError();
+  console.log("🚀 ~ DashboardError ~ error:", error);
 
   return (
     <div>
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        {(error as Error)?.message ||
+          (error as { statusText?: string })?.statusText}
       </p>
     </div>
   );
