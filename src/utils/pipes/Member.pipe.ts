19f2d3fa24cdcbda7a936003082a6b05a0957ba1
2gbeh <tugbeh.osaretin@gmail.com>
2024-02-19 21:40:54 +0100

    add member type

--- src/utils/pipes/Member.pipe.ts
+++ src/utils/pipes/Member.pipe.ts
@@ -2,7 +2,28 @@ import { ETitle } from "@/constants/enums/Person";
 import { EDepartment } from "@/constants/enums/App";
 import EnumHelper from "@/utils/helpers/EnumHelper";
 
-type TMember = Record<string, null | boolean | number | string>;
+// type TMember = Record<string, null | boolean | number | string>;
+type TMember = {
+  id?: string | null;
+  avatar?: string | null;
+  category?: number | null;
+  title?: number | null;
+  surname?: string | null;
+  other_names?: string | null;
+  sex?: number | null;
+  dob?: string | null;
+  marital_status?: number | null;
+  anniversary?: string | null;
+  phone?: string | null;
+  mobile?: string | null;
+  email?: string | null;
+  address?: string | null;
+  occupation?: string | null;
+  house?: number | null;
+  department?: number | null;
+  joined?: string | null;
+  created_at?: string | null;
+};
 
 export default class MemberPipe {
   static intials = (e: TMember) =>
